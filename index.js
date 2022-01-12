1b72c5536985364862a77a978dab109c5fa89ac0
PepperMcLean <jackmcl206@gmail.com>
2022-01-12 13:36:15 -0600

    commit tree 355a76bac2bab494f3e8af76b1bd43f77c751213

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const pets = ["Milo", "Otis", "Garfield"];
+const append = pets.slice();
+const prepend = pets.slice();
+const removeLast = pets.slice();
+const removeFirst = pets.slice();
+append.push("Odie");
+prepend.unshift("Odie");
+removeLast.pop();
+removeFirst.shift();
